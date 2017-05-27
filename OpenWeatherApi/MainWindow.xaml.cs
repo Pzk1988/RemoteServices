4caaefb95356ea5cec026ee348462ba7f6739341
pzk1988 <knapik.przemyslaw@gmail.com>
2017-05-27 14:15:51 +0200

    openWeather + azure recognition

--- OpenWeatherApi/MainWindow.xaml.cs
+++ OpenWeatherApi/MainWindow.xaml.cs
@@ -1,28 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Newtonsoft.Json.Linq;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System;
+using System.Windows.Threading;
 
 namespace OpenWeatherApi
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string inputText { get; set; }
+        public string myApiId { get; set; }
+
+        private string outputText;
+        public string OutputText
+        {
+            get
+            {
+                return outputText;
+            }
+            set
+            {
+                if(outputText != value)
+                {
+                    outputText = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            inputText = "Test";
+            outputText = "Test";
+            myApiId = "18526428403d6156a96193340616fefe";
+            DataContext = this;
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            await Task.Run(() => GetData());
+        }
+
+        private void GetData()
+        {
+            JObject jObject = JObject.Parse(new System.Net.WebClient().
+            DownloadString(string.Format("http://api.openweathermap.org/data/2.5/weather?appid={0}&q={1}", myApiId, inputText)));
+
+            JToken weather = jObject.SelectToken("weather");
+
+            //Application.Current.Dispatcher.BeginInvoke(
+            //    DispatcherPriority.Background,
+            //    new Action(() => outputBox.Text = weather.First.SelectToken("description").ToString()
+            //    )
+            //    );
+            OutputText = weather.First.SelectToken("description").ToString();
+
         }
     }
 }
---
+++ PhotoRecognition/MainWindow.xaml.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.Win32;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace PhotoRecognition
+{
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private string azureRecognitionKey = "1c3d29bde51b4299a715f6e2cd1fa5bf";
+        private string imageSource;
+        public string ImageSource
+        {
+            get
+            {
+                return imageSource;
+            }
+
+            set
+            {
+                if (imageSource != value)
+                {
+                    imageSource = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        private string queryResult;
+        public string QueryResult
+        {
+            get
+            {
+                return queryResult;
+            }
+
+            set
+            {
+                if(queryResult != value)
+                {
+                    queryResult = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        private string query;
+        public string Query
+        {
+            get
+            {
+                return query;
+            }
+
+            set
+            {
+                if (query != value)
+                {
+                    query = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string ImagePath { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                ImageSource = openFileDialog.FileName;
+                MakeAnalysisRequest(ImageSource);
+            }
+
+        }
+
+        async void MakeAnalysisRequest(string imageFilePath)
+        {
+            var client = new HttpClient();
+
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", azureRecognitionKey);
+
+            string requestParameters;
+            if (combo.SelectedIndex == 0)
+            {
+                requestParameters = "details=Landmarks&language=en";
+            }
+            else
+            {
+                requestParameters = "details=Celebrities&language=en";
+            }
+
+            string uri = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze?" + requestParameters;
+            Query = uri;
+
+            HttpResponseMessage response;
+            byte[] byteData = GetImageAsByteArray(imageFilePath);
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                response = await client.PostAsync(uri, content);
+            }
+
+            string temp = await response.Content.ReadAsStringAsync();
+            QueryResult = JsonHelper.FormatJson(temp);
+        }
+
+        byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+            return binaryReader.ReadBytes((int)fileStream.Length);
+        }
+    }
+}
